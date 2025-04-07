f14709347aa0dd5f120b49d8898eb5d9bc3b6680
xlg08 <1146890973@qq.com>
2025-04-07 15:49:41 +0800

    快排结束！

--- 08_排序算法/05_快速排序.py
+++ 08_排序算法/05_快速排序.py
@@ -8,3 +8,25 @@
     @version: 1.0
 '''
 
+
+# 快速排序是冒泡排序的一种改进 ， 具有一些分治法的思想。
+
+def quick_sort(nums):
+    if len(nums) >= 2:
+        mid = nums[len(nums) // 2]
+        left, right = [], []
+        nums.remove(mid)
+        for d in nums:
+            if d < mid:
+                left.append(d)
+            else:
+                right.append(d)
+        return quick_sort(left) + [mid] + quick_sort(right)
+    else:
+        return nums
+
+
+if __name__ == '__main__':
+    nums = [10, 1, 35, 61, 89, 36, 55]
+    nums_new = quick_sort(nums)
+    print(nums_new)
