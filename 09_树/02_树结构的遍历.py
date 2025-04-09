a316946f29b33d70f7af51787ed505cadc29097e
xlg08 <1146890973@qq.com>
2025-04-09 22:46:29 +0800

    树！

--- 09_树/02_树结构的遍历.py
+++ 09_树/02_树结构的遍历.py
@@ -8,7 +8,6 @@
     @version: 1.0
 '''
 
-
 node_list = [
     {'data': 'A', 'left': 'B', 'right': 'C', 'is_root': True},
     {'data': 'B', 'left': 'D', 'right': 'E', 'is_root': False},
@@ -22,12 +21,14 @@ node_list = [
     {'data': 'J', 'left': None, 'right': None, 'is_root': False},
 ]
 
+
 class Node:
     def __init__(self, data, left=None, right=None):
         self.data, self.left, self.right = data, left, right
 
+
 class Tree:
-    def __init__(self, root = None):
+    def __init__(self, root=None):
         self.root = root
 
     def init_data(self, datas):
@@ -36,32 +37,54 @@ class Tree:
 
         # 遍历原数据列表 先封装成node 再将节点进行保存，放到一个容器中，放到一个字典中，字典的键为 node 的值
         for d in datas:
-            node = Node(d['data'], d['left'], d['right'])       # 封装node节点
-            node_dict[d['data']] = node         # 将节点保存到字典中
+            node = Node(d['data'], d['left'], d['right'])  # 封装node节点
+            node_dict[d['data']] = node  # 将节点保存到字典中
 
         # 使节点与节点之间产生关联
         for d in datas:
-            node = node_dict[d['data']]         # 从字典中取出节点
+            node = node_dict[d['data']]  # 从字典中取出节点
 
-            if node.left:       # 如果存在左子节点
-                node.left = node_dict[node.left]        # 将子节点挂到父节点左边
-            if node.right:      # 如果存在右子节点
-                node.right = node_dict[node.right]      # 将子节点挂到父节点右边
-            if d['is_root']:        # 如果是根节点，则将该节点赋给根
+            if node.left:  # 如果存在左子节点
+                node.left = node_dict[node.left]  # 将子节点挂到父节点左边
+            if node.right:  # 如果存在右子节点
+                node.right = node_dict[node.right]  # 将子节点挂到父节点右边
+            if d['is_root']:  # 如果是根节点，则将该节点赋给根
                 self.root = node
 
-
+    # 深度优先 先序遍历
     def iter_node1(self, node):
+        if node:      # 当还存在节点时执行
+            # print(node)     # 打印当前节点
+            print(node.data, end='\t')      # 打印当前节点的值
+            self.iter_node1(node.left)      # 将该节点的左节点再作为根调用函数
+            self.iter_node1(node.right)     # 将该节点的右节点再作为根调用函数
+            # print()
+
+
+    # 广度优先
+    def iter_node2(self, node):
+
+        node_list = [node]      # 先传入的根节点，会先将根节点放入到列表中
+        for n in node_list:     # 遍历列表中的节点，以及在循环中添加到列表中的节点
+            print(n.data)       # 输出当前节点的值
+            if n.left:    # 当存在左子节点时，将子节点添加到列表中
+                node_list.append(n.left)
+            if n.right:     # 当存在右子节点时，将子节点添加到列表中
+                node_list.append(n.right)
+
+
+    def reverse(self, node):
         if node:
-            print(node)
-            print(node.data, end='\t')
-            self.iter_node1(node.left)
-            self.iter_node1(node.right)
-            print()
+            node.left, node.right = node.right, node.left
+            self.reverse(node.left)
+            self.reverse(node.right)
+
 
 
 if __name__ == '__main__':
-
     tree = Tree()
     tree.init_data(node_list)
-    tree.iter_node1(tree.root)
+    # tree.iter_node1(tree.root)
+    # tree.iter_node2(tree.root)
+    tree.reverse(tree.root)
+    tree.iter_node2(tree.root)
---
+++ 10_堆/01_最大堆的实现.py
@@ -0,0 +1,64 @@
+'''
+    @User: LG
+    @Auther: http://www.xlonggang.cn
+    @Date: 2025/4/9 0009 22:32 
+    @Description: 
+    @Project ：数据结构 
+    @File    ：01_最大堆的实现.py
+    @version: 1.0
+'''
+class Array:
+    def __init__(self, size=4):
+        self.__size = size      # 记录容器的大小
+        self.__item = [None] * size  # 分配空间
+        self.__length = 0
+
+    # 向数组中添加元素
+    def __setitem__(self, key, value):
+        self.__item[key] = value
+        self.__length += 1
+
+    # 从数组中获得元素
+    def __getitem__(self, key):
+        return self.__item[key]
+
+    # 返回数组的大小
+    def __len__(self):
+        return self.__length
+
+    def __iter__(self):
+        for value in self.__item:
+            yield value
+
+
+class Heap:
+    def __init__(self):
+        self.item = Array(8)
+        self.count = 0
+
+    def add(self, value):
+        self.item[self.count] = value
+        self.siftup(self.count)
+        self.count += 1
+
+
+    def siftup(self, index):        # 与父结点进行比较，实现换值效果
+        if index > 0:       # 当不是根节点就进行比较
+            parent = (index-1)//2           # 找到该节点的父节点的索引
+            if self.item[index] > self.item[parent]:        # 如果当前节点比父节点小则进行交换
+                self.item[index], self.item[parent] = self.item[parent], self.item[index]
+                # 当前元素与父节点交换完之后，则将当前节点再向上进行比较，只到不比对应的父节点大，或已经成为根节点
+                self.siftup(parent)
+
+
+
+if __name__ == '__main__':
+    heap = Heap()
+    heap.add(10)
+    heap.add(15)
+    heap.add(20)
+    heap.add(14)
+
+    for i in heap:
+        if i:
+            print(i)
